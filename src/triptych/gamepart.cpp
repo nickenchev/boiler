7ab943e2985e2b6a8fa23b07da5f3ef881607925
Nick Enchev <nikolaenchev@gmail.com>
2016-01-23 16:29:41 -0500

    Added triptych gamepart and set old one aside

---
+++ src/triptych/gamepart.cpp
@@ -0,0 +1,1 @@
+#include "gamepart.h"
---
+++ src/triptych/gamepart.h
@@ -0,0 +1,14 @@
+#ifndef GAMEPART_H
+#define GAMEPART_H
+
+#include "part.h"
+
+class GamePart : Part
+{
+public:
+    GamePart();
+    virtual ~GamePart();
+};
+
+
+#endif /* GAMEPART_H */
