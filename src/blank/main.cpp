238fdc6009534193d72a376ca9c48faf658f371f
Nick Enchev <nikolaenchev@gmail.com>
2017-03-25 00:09:28 -0400

    Tweaking

--- src/blank/main.cpp
+++ src/blank/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-    Engine::getInstance().initialize(std::make_unique<OpenGLRenderer>(false), 512, 512);
+	Engine::getInstance().initialize(std::make_unique<OpenGLRenderer>(false), 512, 512);
 
     auto part = std::make_shared<BlankPart>();
     Engine::getInstance().start(part);
