fcbe1a0a880bb04f20bcefb5830e2d26b9e29ce1
Nick Enchev <nikolaenchev@gmail.com>
2017-03-25 00:18:58 -0400

    Tabs

--- src/blank/main.cpp
+++ src/blank/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	Engine::getInstance().initialize(std::make_unique<OpenGLRenderer>(false), 512, 512);
 
 	auto part = std::make_shared<BlankPart>();
-    Engine::getInstance().start(part);
+	Engine::getInstance().start(part);
 
-    return 0;
+	return 0;
 }
