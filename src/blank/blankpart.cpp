f7598137f488413531a09eac906c6ceb96db3e53
Nick Enchev <nikolaenchev@gmail.com>
2017-08-09 16:35:19 -0400

    Generating a bunch of tiles

--- src/blank/blankpart.cpp
+++ src/blank/blankpart.cpp
@@ -12,7 +12,7 @@ void BlankPart::onStart()
 {
     spriteSheet = Boiler::getInstance().getSpriteLoader().loadSheet("data/terrain.json");
 
-    Boiler::getInstance().getRenderer().setClearColor(Color3(0.8f, 0.8f, 1.0f));
+    Boiler::getInstance().getRenderer().setClearColor(Color3(0, 0, 0));
     Boiler::getInstance().addKeyInputListener([](const KeyInputEvent &event)
 	{
 		if (event.keyCode == SDLK_ESCAPE)
@@ -29,10 +29,15 @@ void BlankPart::onStart()
 		.expects<SpriteComponent>(ecs.getComponentMapper());
 
 	// create our entity and setup its components
-	Entity entity = ecs.newEntity();
-    auto pos = ecs.addComponent<PositionComponent>(entity, Rect(10, 10, 100, 100));
-	pos->absolute = true;
-
-	auto sprite = ecs.addComponent<SpriteComponent>(entity, pos->frame);
-	sprite->spriteFrame = spriteSheet->getFrame("forest.png");
+	for (int r = 0; r < 10; ++r)
+	{
+		for (int c = 0; c < 10; ++c)
+		{
+			Entity entity = ecs.newEntity();
+			auto pos = ecs.addComponent<PositionComponent>(entity, Rect(100 * r, 100 * c, 100, 100));
+			pos->absolute = true;
+			auto sprite = ecs.addComponent<SpriteComponent>(entity, pos->frame);
+			sprite->spriteFrame = spriteSheet->getFrame("forest.png");
+		}
+	}
 }
