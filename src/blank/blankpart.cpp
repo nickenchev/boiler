f5be00c3de1933af4c23d0473ee817dc0ec4ca81
Nick Enchev <nikolaenchev@gmail.com>
2017-08-09 00:50:27 -0400

    Functional ECS with rendering!

--- src/blank/blankpart.cpp
+++ src/blank/blankpart.cpp
@@ -10,6 +10,8 @@ BlankPart::BlankPart() : logger("Playground Part")
 
 void BlankPart::onStart()
 {
+    spriteSheet = Boiler::getInstance().getSpriteLoader().loadSheet("data/terrain.json");
+
     Boiler::getInstance().getRenderer().setClearColor(Color3(0.8f, 0.8f, 1.0f));
     Boiler::getInstance().addKeyInputListener([](const KeyInputEvent &event)
 	{
@@ -29,5 +31,8 @@ void BlankPart::onStart()
 	// create our entity and setup its components
 	Entity entity = ecs.newEntity();
     auto pos = ecs.addComponent<PositionComponent>(entity, Rect(10, 10, 100, 100));
+	pos->absolute = true;
+
 	auto sprite = ecs.addComponent<SpriteComponent>(entity, pos->frame);
+	sprite->spriteFrame = spriteSheet->getFrame("forest.png");
 }
--- src/blank/blankpart.h
+++ src/blank/blankpart.h
@@ -1,13 +1,17 @@
 #ifndef BLANKPART_H
 #define BLANKPART_H
 
+#include <memory>
 #include <boiler.h>
 #include "core/part.h"
 #include "core/logger.h"
 
+class SpriteSheet;
+
 class BlankPart : public Part
 {
 	Logger logger;
+    std::shared_ptr<const SpriteSheet> spriteSheet;
 	
 public:
     BlankPart();
