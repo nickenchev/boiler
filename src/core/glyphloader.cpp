fed53d2d043277e89099f67c903b7fcccee492aa
Nick Enchev <nikolaenchev@gmail.com>
2019-02-05 11:13:25 -0500

    Basic texture packing loader

--- src/core/glyphloader.cpp
+++ src/core/glyphloader.cpp
@@ -1,4 +1,5 @@
 #include <boiler.h>
+#include <video/opengl.h>
 #include "core/glyphloader.h"
 
 GlyphLoader::GlyphLoader() : logger("Glyph Loader")
@@ -41,34 +42,64 @@ void GlyphLoader::loadFace(std::string fontPath)
 	auto cmprs = [](FT_Glyph glyph1, FT_Glyph glyph2) {
 		FT_BitmapGlyph bmg1 = (FT_BitmapGlyph)glyph1;
 		FT_BitmapGlyph bmg2 = (FT_BitmapGlyph)glyph2;
-		int area1 = bmg1->bitmap.width * bmg1->bitmap.rows;
-		int area2 = bmg2->bitmap.width * bmg2->bitmap.rows;
-		return area1 < area2;
+		//int area1 = bmg1->bitmap.width * bmg1->bitmap.rows;
+		//int area2 = bmg2->bitmap.width * bmg2->bitmap.rows;
+
+		return bmg1->bitmap.rows < bmg2->bitmap.rows;
 	};
 	logger.log("Building glyph atlas");
 	std::sort(glyphs.begin(), glyphs.end(), cmprs);
+	std::reverse(glyphs.begin(), glyphs.end());
+
+	FT_BitmapGlyph tallestGlyph = (FT_BitmapGlyph)glyphs[0];
+	GLfloat atlasHeight = tallestGlyph->bitmap.rows;
+	GLfloat atlasWidth = 0;
+	for (FT_Glyph glyph : glyphs)
+	{
+		FT_BitmapGlyph bmg = (FT_BitmapGlyph)glyph;
+		atlasWidth += bmg->bitmap.width;
+	}
+
+	// let opengl store textures that are not multiple of 4
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	// create opengl texture
+	GLuint texture = 0;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, atlasWidth, atlasHeight, 0, GL_RED, GL_UNSIGNED_BYTE, nullptr);
+
+	// write bitmap data to atlas
+	GLfloat xOffset = 0;
+	GLfloat yOffset = 0;
+	for (FT_Glyph glyph : glyphs)
+	{
+		FT_BitmapGlyph bmg = (FT_BitmapGlyph)glyph;
+		glTexSubImage2D(GL_TEXTURE_2D, 0, xOffset, yOffset, bmg->bitmap.width, bmg->bitmap.rows, GL_RED, GL_UNSIGNED_BYTE, bmg->bitmap.buffer);
+		xOffset += bmg->bitmap.width;
+	}
+
+	GLint swizzleMask[] = { GL_ONE, GL_ONE, GL_ONE, GL_RED };
+	glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	logger.log("Loaded font at: " + fontPath);
+
+	// clean up FT resources
 	FT_Done_Face(face);
 	for (FT_Glyph glyph : glyphs)
 	{
 		FT_Done_Glyph(glyph);
 	}
 
-	// let opengl store textures that are not multiple of 4
-	//glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 			/*
 			GLuint texture = 0;
 			glGenTextures(1, &texture);
 			glBindTexture(GL_TEXTURE_2D, texture);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width, face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, face->glyph->bitmap.buffer);
-			GLint swizzleMask[] = { GL_ONE, GL_ONE, GL_ONE, GL_RED };
-			glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
-
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 			Glyph glyph = {
 				texture,
