52e358ec3e1cd865b2945251408cf0ccd05fb103
Nick Enchev <nikolaenchev@gmail.com>
2019-04-22 00:20:16 -0400

    Added support for parent positions

--- src/video/systems/glyphsystem.cpp
+++ src/video/systems/glyphsystem.cpp
@@ -11,6 +11,12 @@ void GlyphSystem::update(ComponentStore &store, const double delta) const
 		PositionComponent &pos = store.retrieve<PositionComponent>(entity);
 		TextComponent &text = store.retrieve<TextComponent>(entity);
 
+		// offset by parent's position
+		if (pos.parent)
+		{
+			pos.frame.position = pos.parent->frame.position;
+		}
+
 		assert(text.glyphMap != nullptr);
 		Boiler::getInstance().getRenderer().render(pos, text);
 	}
