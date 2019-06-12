9284b83955987a75c025f0a621d57be3c76fafda
Nick Enchev <nikolaenchev@gmail.com>
2019-06-12 18:35:50 -0400

    TextureInfo now cleans up tex coords VBO

--- src/video/glyphmap.cpp
+++ src/video/glyphmap.cpp
@@ -5,13 +5,4 @@ using namespace Boiler;
 
 GlyphMap::~GlyphMap()
 {
-	for (auto itr : map)
-	{
-		const Glyph &glyph = itr.second;
-		if (glyph.getTexCoordsVbo())
-		{
-			GLuint vbo = glyph.getTexCoordsVbo();
-			glDeleteBuffers(1, &vbo);
-		}
-	}
 }
