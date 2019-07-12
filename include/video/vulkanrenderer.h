38ed1ecbc3ea1a7b462e410d7d00e0508f3f2205
Nick Enchev <nikolaenchev@gmail.com>
2019-07-12 13:32:10 -0400

    Vulkan Renderer class setup

---
+++ include/video/vulkanrenderer.h
@@ -0,0 +1,36 @@
+#ifndef VULKANRENDERER_H
+#define VULKANRENDERER_H
+
+#include <string>
+#include "video/renderer.h"
+
+namespace Boiler
+{
+
+class VulkanRenderer : public Boiler::Renderer
+{
+public:
+    VulkanRenderer();
+
+	void initialize(const Boiler::Size &size) override;
+	void shutdown() override;
+	std::string getVersion() const override;
+
+    std::shared_ptr<const Texture> createTexture(const std::string filePath, const Size &textureSize, const void *pixelData) const override;
+    void setActiveTexture(std::shared_ptr<const Texture> texture) const override;
+
+    std::shared_ptr<const Model> loadModel(const VertexData &data) const override;
+
+	void beginRender() override;
+	void endRender() override;
+
+	void render(const glm::mat4 modelMatrix, const std::shared_ptr<const Model> model,
+				const std::shared_ptr<const Texture> sourceTexture, const TextureInfo *textureInfo,
+				const glm::vec4 &colour) const override;
+
+    void showMessageBox(const std::string &title, const std::string &message) override;
+};
+
+}
+
+#endif /* VULKANRENDERER_H */
---
+++ src/video/vulkanrenderer.cpp
@@ -0,0 +1,51 @@
+#include "video/vulkanrenderer.h"
+
+using namespace Boiler;
+
+VulkanRenderer::VulkanRenderer() : Renderer("Vulkan Renderer")
+{
+}
+
+void VulkanRenderer::initialize(const Size &size)
+{
+}
+
+void VulkanRenderer::shutdown()
+{
+}
+
+std::string VulkanRenderer::getVersion() const
+{
+	return "Boiler Vulkan 1.0";
+}
+
+std::shared_ptr<const Texture> VulkanRenderer::createTexture(const std::string filePath, const Size &textureSize, const void *pixelData) const
+{
+	return nullptr;
+}
+
+void VulkanRenderer::setActiveTexture(std::shared_ptr<const Texture> texture) const
+{
+}
+
+std::shared_ptr<const Model> VulkanRenderer::loadModel(const VertexData &data) const
+{
+	return nullptr;
+}
+
+void VulkanRenderer::beginRender()
+{
+}
+void VulkanRenderer::endRender()
+{
+}
+
+void VulkanRenderer::render(const glm::mat4 modelMatrix, const std::shared_ptr<const Model> model,
+							const std::shared_ptr<const Texture> sourceTexture, const TextureInfo *textureInfo,
+							const glm::vec4 &colour) const
+{
+}
+
+void VulkanRenderer::showMessageBox(const std::string &title, const std::string &message)
+{
+}
