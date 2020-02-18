a6719399a615b0c0f552e5d60264e392c927bc5b
Nick Enchev <nikolaenchev@gmail.com>
2020-02-17 23:19:41 -0500

    Started cam and transform system

---
+++ include/core/components/transformcomponent.h
@@ -0,0 +1,16 @@
+#ifndef TRANSFORMCOMPONENT_H
+#define TRANSFORMCOMPONENT_H
+
+#include "core/componenttype.h"
+
+namespace Boiler
+{
+
+class TransformComponent : public ComponentType<TransformComponent>
+{
+public:
+};
+
+}
+
+#endif /* TRANSFORMCOMPONENT_H */
