faa8d4ccf3e1a069bec7f78a63fcb45a6c022804
Nick Enchev <nikolaenchev@gmail.com>
2020-06-24 18:24:47 -0400

    Fixed rotation and model matrix calculation

--- include/core/components/positioncomponent.h
+++ include/core/components/positioncomponent.h
@@ -2,6 +2,8 @@
 #define POSITIONCOMPONENT_H
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
 #include "core/componenttype.h"
 #include "core/rect.h"
 #include "core/math.h"
@@ -12,19 +14,15 @@ namespace Boiler
 struct PositionComponent : public ComponentType<PositionComponent>
 {
     Rect frame;
-	vec3 rotationAxis;
-	float rotationAngle;
-    bool flipH, flipV, collides;
     vec3 scale;
+	vec4 rotation;
     bool absolute;
 
-	PositionComponent(Rect frame) : frame(frame), rotationAxis(0, 0, 0), scale(1.0f, 1.0f, 1.0f)
+	PositionComponent(Rect frame) : frame(frame)
 	{
-		flipH = false;
-		flipV = false;
-		collides = false;
+		scale = {1.0f, 1.0f, 1.0f};
+		rotation = {0, 0, 0, 0};
 		absolute = false;
-		rotationAngle = 0;
 	}
 
 	const mat4 getMatrix() const
@@ -34,23 +32,11 @@ struct PositionComponent : public ComponentType<PositionComponent>
 					  frame.position.y - frame.size.height * frame.pivot.y,
 					  frame.position.z);
 
-		float scaleX = scale.x;
-		float scaleY = scale.y;
-		if (flipH)
-		{
-			pivotPos.x += frame.size.width;
-			scaleX *= -1;
-		}
-		if (flipV)
-		{
-			pivotPos.x += frame.size.height;
-			scaleY *= -1;
-		}
 		// create the model matrix, by getting a 3D vector from the Entity's vec2 position
-		mat4 modelMatrix;
-		modelMatrix = translate(mat4(1), pivotPos);
-		modelMatrix = glm::rotate(modelMatrix, glm::radians(rotationAngle), rotationAxis);
-		modelMatrix = glm::scale(modelMatrix, vec3(scaleX, scaleY, scale.z));
+		mat4 scaleMatrix = glm::scale(mat4(1), vec3(scale.x, scale.y, scale.z));
+		mat4 rotationMatrix = glm::mat4(glm::qua(rotation.w, rotation.x, rotation.y, rotation.z));
+		mat4 translationMatrix = translate(mat4(1), pivotPos);
+		mat4 modelMatrix = translationMatrix * rotationMatrix * scaleMatrix;
 
 		return modelMatrix;
 	}
