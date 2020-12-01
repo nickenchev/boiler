468b573932e9a8d643d177cd5cb3060f01a55306
Nick Enchev <nikolaenchev@gmail.com>
2020-12-01 15:52:54 -0500

    added mouse motion event constructor

--- include/input/mousemotionevent.h
+++ include/input/mousemotionevent.h
@@ -7,6 +7,8 @@ namespace Boiler
 struct MouseMotionEvent
 {
     float x, y, xDistance, yDistance;
+
+	MouseMotionEvent() : MouseMotionEvent(0, 0, 0, 0) {}
     MouseMotionEvent(float x, float y, float xDistance, float yDistance)
     {
         this->x = x;
