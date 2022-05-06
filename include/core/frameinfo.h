2ec1fbbdaf235fb49c3f6e857e3bd312a217248a
Nick Enchev <nikolaenchev@gmail.com>
2022-05-06 21:38:05 +0300

    Adding input events to frame info

--- include/core/frameinfo.h
+++ include/core/frameinfo.h
@@ -1,13 +1,18 @@
 #ifndef FRAMEINFO_H
 #define FRAMEINFO_H
 
+#include <array>
 #include "core/common.h"
+#include "input/inputevent.h"
 
 namespace Boiler
 {
 
+constexpr size_t maxInputEvents = 32;
+
 struct FrameInfo
 {
+	std::array<InputEvent, maxInputEvents> inputEvents;
 	short currentFrame;
 	Time deltaTime, globalTime;
 };
