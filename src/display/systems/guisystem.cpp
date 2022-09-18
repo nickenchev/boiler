4a9697fc9ea453081af0205dad19155af944101e
Nick Enchev <nikolaenchev@gmail.com>
2022-09-18 16:56:31 +0300

    Alpha tweak

--- src/display/systems/guisystem.cpp
+++ src/display/systems/guisystem.cpp
@@ -14,7 +14,7 @@ GUISystem::GUISystem(Renderer &renderer) : System("ImGui System")
 	logger.log("Initializing");
 	ImGui::CreateContext();
 
-	ImGui::GetStyle().Alpha = 0.7f;
+	ImGui::GetStyle().Alpha = 0.9f;
 
 	ImGuiIO &io = ImGui::GetIO();
 
